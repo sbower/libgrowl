e2313e69caee1d82916779f27a23cc366b23ce08
Shawn Bower <shawn.bower@gmail.com>
2010-10-02 17:31:24 -0400

    refactoreing

---
+++ src/test/java/net/sf/libgrowl/GNTPTest.java
@@ -0,0 +1,45 @@
+package net.sf.libgrowl;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GNTPTest {
+	
+	private java.util.List emptyList; 
+
+	/** 
+     * Sets up the test fixture. 
+     * (Called before every test case method.) 
+     */ 
+    @Before 
+    public void setUp() { 
+        emptyList = new java.util.ArrayList(); 
+    } 
+
+
+    /** 
+     * Tears down the test fixture. 
+     * (Called after every test case method.) 
+     */ 
+    @After 
+    public void tearDown() { 
+        emptyList = null; 
+    } 
+
+
+    @Test 
+    public void testSomeBehavior() { 
+        assertEquals("Empty list should have 0 elements", 0, emptyList.size()); 
+    } 
+
+    @Test(expected=IndexOutOfBoundsException.class) 
+    public void testForException() { 
+        Object o = emptyList.get(0); 
+    } 
+
+
+}
